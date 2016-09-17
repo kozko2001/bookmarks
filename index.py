307eb9316d45b6553078b640a4e0c26e76ce817a
Jordi Coscolla <jordi@coscolla.net>
2016-09-17 18:18:32 +0200

    first version that updates a constant note

---
+++ index.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, abort
+from simplenote import Simplenote
+import json
+
+username = ''
+password = ''
+
+with open('config.json') as data_file:    
+    data = json.load(data_file)
+    username = data['username']
+    password = data['password']
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def post():
+    s = Simplenote(username, password) 
+    (notes, result) = s.get_note_list(tags=['bookmarks'])
+    if result == 0: ## sucess
+        bookmarknote = notes[0]
+        
+        (note, result) = s.get_note(bookmarknote['key'])
+        content = note['content']
+        
+        content = '%s\n\n%s' % (content, request.data.decode('utf-8'))
+        bookmarknote['content'] = content
+
+        s.update_note(bookmarknote)
+        return 'OK'
+    abort(404)
+
+if __name__ == '__main__':
+    app.run()
